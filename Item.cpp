f86b3dd71b9c85191c285c83db647c34d4dfc304
PranavNaugain <pranav.naugain@gmail.com>
2023-01-09 23:34:41 +0530

    Item Basic C++ Code

---
+++ Item.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Item.h"
+
+// Sets default values
+AItem::AItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("BEGIN PLAY CALLED !!"));
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.0f, FColor::Cyan, FString("Item OnScreen Message !"));
+	}
+	
+
+}
+// Called every frame
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	UE_LOG(LogTemp, Warning, TEXT("Deltatime : %f "), DeltaTime);
+	if (GEngine)
+	{
+		FString Message = FString::Printf(TEXT("DeltaTime : %f "), DeltaTime);
+		GEngine->AddOnScreenDebugMessage(1, 60.0f, FColor::Cyan, Message);
+	}
+
+}
+
